9700c4f20fa6fb618398373990d10ff890510f49
Paolo Matchett <paolomatchett@gmail.com>
2021-03-19 14:07:22 -0500

    Fix issues with checkout

--- mutations/checkout.ts
+++ mutations/checkout.ts
@@ -1,9 +1,13 @@
-/* eslint-disable */
-import { KeystoneContext } from "@keystone-next/types";
+import { CartItem } from "../schemas/CartItem";
+import { User } from "../schemas/User";
 import {
   CartItemCreateInput,
   OrderCreateInput,
 } from "../.keystone/schema-types";
+
+/* eslint-disable */
+import { KeystoneContext, SessionStore } from "@keystone-next/types";
+import stripeConfig from "../lib/stripe";
 
 const graphql = String.raw;
 
@@ -16,12 +20,12 @@ async function checkout(
   { token }: Arguments,
   context: KeystoneContext
 ): Promise<OrderCreateInput> {
-  //1. Make sure they are signed in
+  // 1. Make sure they are signed in
   const userId = context.session.itemId;
   if (!userId) {
-    throw new Error("Sorry! You must be signed in tho create an order!");
+    throw new Error("Sorry! You must be signed in to create an order!");
   }
-  //1.5 Query the current user
+  // 1.5 Query the current user
   const user = await context.lists.User.findOne({
     where: { id: userId },
     resolveFields: graphql`
@@ -39,6 +43,7 @@ async function checkout(
           photo {
             id
             image {
+              id
               publicUrlTransformed
             }
           }
@@ -46,18 +51,18 @@ async function checkout(
       }
     `,
   });
-  console.dir(user);
-  //2. Calc the total price for their order
+  console.dir(user, { depth: null });
+  // 2. calc the total price for their order
   const cartItems = user.cart.filter((cartItem) => cartItem.product);
   const amount = cartItems.reduce(function (
     tally: number,
     cartItem: CartItemCreateInput
   ) {
-    return tally + cartItem * cartItem.product.price;
+    return tally + cartItem.quantity * cartItem.product.price;
   },
   0);
   console.log(amount);
-  //3. Create the payment with the stripe library
+  // 3. create the charge with the stripe library
   const charge = await stripeConfig.paymentIntents
     .create({
       amount,
@@ -69,8 +74,34 @@ async function checkout(
       console.log(err);
       throw new Error(err.message);
     });
-  //4. Convert ther cartItems to OrderItems
-  //5. Create the order and return it
+  console.log(charge);
+  // 4. Convert the cartItems to OrderItems
+  const orderItems = cartItems.map((cartItem) => {
+    const orderItem = {
+      name: cartItem.product.name,
+      description: cartItem.product.description,
+      price: cartItem.product.price,
+      quantity: cartItem.quantity,
+      photo: { connect: { id: cartItem.product.photo.id } },
+    };
+    return orderItem;
+  });
+  // 5. Create the order and return it
+  const order = await context.lists.Order.createOne({
+    data: {
+      total: charge.amount,
+      charge: charge.id,
+      items: { create: orderItems },
+      user: { connect: { id: userId } },
+    },
+    resolveFields: false,
+  });
+  // 6. Clean up any old cart item
+  const cartItemIds = cartItems.map((cartItem) => cartItem.id);
+  await context.lists.CartItem.deleteMany({
+    ids: cartItemIds,
+  });
+  return order;
 }
 
 export default checkout;
